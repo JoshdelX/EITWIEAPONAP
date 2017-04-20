8020648a6acc4234f02d0154043268ad382679c6
JonathanQuang <jquang@stuy.edu>
2017-04-20 07:42:12 -0400

    ticket

---
+++ Ticket.java
@@ -0,0 +1,27 @@
+
+public class Ticket implements Comparable {
+    private int ID;
+    public String desc;
+    private int priority;
+    private String username;
+
+    //>>>>>>>>>>>> accessors <<<<<<<<<<<<<<<
+
+    public int getID() {
+	return ID;
+    }
+
+    public int getPriority() {
+	return priority;
+    }
+
+    public String getUsername() {
+	return username;
+    }
+
+
+    public int setID() {
+    
+    }
+
+}
