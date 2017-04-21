fb7d1f86c6c5a891955740ca5eaccfec5db15d3b
JoshdelX <sxiedeng@stuy.edu>
2017-04-21 05:41:54 -0400

    working on calculate method

--- HelpDesk.java
+++ HelpDesk.java
@@ -1,11 +1,36 @@
 public class HelpDesk {
 
     private ArrayPriorityQueue tickets;
+    private ArrayList keyWords;
 
+    //>>>>>>>>>>>>>>> CONSTRUCTOR <<<<<<<<<<<<<<<
     public HelpDesk() {
 	tickets = new ArrayPriorityQueue();
+	keyWords = new ArrayList();
+	keyWords.add("unplugged");
+	keyWords.add("BSOD");
+	keyWords.add("broken");
+	//keyWords.add("word");
     }
 
-    //>>>>> INSERT METHODS HERE <<<<<
+    //>>>>>>>>>>>>>>> METHODS <<<<<<<<<<<<<<<
+    public calculate(Ticket x) {
+	x.setPriority(x.getID());
+	String temp = x.getDesc();
+	String temp2 = "";
+	ArrayList stor = new ArrayList();
+	for (int n = 0; n < temp.length(); n++ ) {
+	    if (temp.substring(n, n+1) == " ") {
+		stor.add(temp2);
+	    }
+	    else {
+
+	    }
+	}
+    }
+
+    public static void main(String[] args) {
+
+    }
 
 }
