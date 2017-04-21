5509ff58a077192d39d73fe94a19f0afc453407c
JoshdelX <sxiedeng@stuy.edu>
2017-04-21 05:12:35 -0400

    created HelpDesk

---
+++ HelpDesk.java
@@ -0,0 +1,11 @@
+public class HelpDesk {
+
+    private ArrayPriorityQueue tickets;
+
+    public HelpDesk() {
+	tickets = new ArrayPriorityQueue();
+    }
+
+    //>>>>> INSERT METHODS HERE <<<<<
+
+}
