7ddcee7a775e26600c2740a5fc4bf37b2de3d0f6
JonathanQuang <jquang@stuy.edu>
2017-04-21 07:25:26 -0400

    made ticket comparable and added mutators

--- Ticket.java
+++ Ticket.java
@@ -1,12 +1,19 @@
-
-public class Ticket implements Comparable {
+public class Ticket implements Comparable<Ticket> {
+  
     private int ID;
     public String desc;
     private int priority;
     private String username;
 
-    //>>>>>>>>>>>> accessors <<<<<<<<<<<<<<<
+    //>>>>>>>>>>>>>> CONSTRUCTOR <<<<<<<<<<<<<<<
+    public Ticket(int theID, String theDesc, int thePrior, String theUser){
+	ID = theID;
+	desc = theDesc;
+	priority = thePrior;
+	username = theUser;
+    }
 
+    //>>>>>>>>>>>>>>> ACCESSORS <<<<<<<<<<<<<<<
     public int getID() {
 	return ID;
     }
@@ -19,9 +26,28 @@ public class Ticket implements Comparable {
 	return username;
     }
 
-
-    public int setID() {
+    //>>>>>>>>>>>>>>> MUTATORS <<<<<<<<<<<<<<<
+    public void setID(int x) {
+	ID = x;
+    }
     
+    public void setPriority(int x) {
+	priority = x;
+    }
+
+    public void setUsername(String x) {
+	username = x;
+    }
+
+    //>>>>>>>>>> OTHER METHODS <<<<<<<<<<
+    public int compareTo(Ticket x) {
+	if (priority < x.getPriority()){
+	    return -1;
+	}
+	else if (priority > x.getPriority()){
+	    return 1;
+	}
+	return 0;
     }
 
 }
